5627e59b351162438f5f2f57e1254922212db4a6
Jacob Page <jacob.h.page@gmail.com>
2017-07-21 22:36:06 -0700

    Save memory with prototypes
    Currently, this implementation is really inefficient and is probably leading to circular references the GC can't untangle. Each instance of a data set is being created with its own unique method objects, referencing a cache inside of a closure. With these changes, now each data source creates all methods in one prototype, and each data set is just an object with a cache property deriving from that prototype.

--- lib/lazy-bones.ts
+++ lib/lazy-bones.ts
@@ -1,12 +1,11 @@
 import { DataSourceSpec, DataSetConstructor } from './model';
 import Orchestrator from './orchestrator';
+import Orchestration from './orchestration';
 
 function LazyBones<T, U>(spec: DataSourceSpec<T, U>) : DataSetConstructor<T, U> {
   const orchestrator = new Orchestrator(spec);
 
-  let constructor = data => orchestrator.getInstance(data);
-
-  return Object.setPrototypeOf(constructor, orchestrator.emitter);
+  return Object.setPrototypeOf(Orchestration(orchestrator), orchestrator.emitter);
 }
 
 export default LazyBones;
